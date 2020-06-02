53c10225215163984d9f868ef6dd8258bdac7659
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2020-06-02 18:26:55 -0300

    funcionando

--- app/agenda/views.py
+++ app/agenda/views.py
@@ -1,47 +1,30 @@
 from django.shortcuts import render
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from agenda.models import Contato
 from agenda.serializers import ContatoSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class AgendaList(APIView):
-  def get(self, request, format=None):
-    contato = Contato.objects.all()
-    serializer = ContatoSerializer(contato, many=True)
-    return Response(serializer.data)
+class AgendaList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+  queryset = Contato.objects.all()
+  serializer_class = ContatoSerializer
 
-  def post(self, request, format=None):
-    serializer = ContatoSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  def get(self, request, *args, **kwargs):
+    return self.list(request, *args, **kwargs)
+
+  def post(self, request, *args, **kwargs):
+    return self.create(request, *args, **kwargs)
 
 
-class AgendaDetail(APIView):
-  def get_object(self, pk):
-    try:
-      return Contato.objects.get(pk=pk)
-    except Contato.DoesNotExist:
-      raise Http404
+class AgendaDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+  queryset = Contato.objects.all()
+  serializer_class = ContatoSerializer
 
-  def get(self, request, pk, format=None):
-    contato = self.get_object(pk)
-    serializer = ContatoSerializer(contato)
-    return Response(serializer.data)
+  def get(self, request, *args, **kwargs):
+    return self.retrieve(request, *args, **kwargs)
 
-  def put(self, request, pk, format=None):
-    contato = self.get_object(pk)
-    serializer = ContatoSerializer(contato, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  def put(self, request, *args, **kwargs):
+    return self.update(request, *args, **kwargs)
 
-  def delete(self, request, pk, format=None):
-    contato = self.get_object(pk)
-    contato.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+  def delete(self, request, *args, **kwargs):
+    return self.destroy(request, *args, **kwargs)
