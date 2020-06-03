5d86e4f562209c12f414c42ec9dbea02cb6d7714
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2020-06-03 19:35:11 -0300

    versão final

--- app/agenda/views.py
+++ app/agenda/views.py
@@ -1,30 +1,14 @@
 from django.shortcuts import render
 from agenda.models import Contato
 from agenda.serializers import ContatoSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class AgendaList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class AgendaList(generics.ListCreateAPIView):
   queryset = Contato.objects.all()
   serializer_class = ContatoSerializer
 
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
 
-  def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
-
-
-class AgendaDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class AgendaDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Contato.objects.all()
   serializer_class = ContatoSerializer
-
-  def get(self, request, *args, **kwargs):
-    return self.retrieve(request, *args, **kwargs)
-
-  def put(self, request, *args, **kwargs):
-    return self.update(request, *args, **kwargs)
-
-  def delete(self, request, *args, **kwargs):
-    return self.destroy(request, *args, **kwargs)
